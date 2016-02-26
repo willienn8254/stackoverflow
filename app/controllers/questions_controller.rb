a7c8ea38d29b948ad9c5e6ac1aca05f0fc23972b
willienn8254 <willienn@hotmail.com>
2016-02-25 21:54:30 -0500

    Publica preguntas

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,45 @@ class QuestionsController < ApplicationController
 
 	def index
 
+		@questions=Question.all
+
 	end
+
+
+	def new
+
+		@question=Question.new
+
+	end
+
+
+	def create
+
+		@question=Question.new(question_params)
+
+		if @question.save
+
+			redirect_to questions_path, notice: "La pregunta fue publicada con exito"
+
+		else
+
+			render :new
+
+		end
+
+	end
+
+
+
+
+
+
+	private
+
+	def question_params
+
+		params.require(:question).permit(:title, :body)
+
+	end
+
 end
