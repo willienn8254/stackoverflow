3d958ad9b3f5e6610caa83c63f7404f2173624e0
willienn8254 <willienn@hotmail.com>
2016-03-17 22:11:30 -0500

    Implementa funcionalidad votar respuesta

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -17,6 +17,10 @@ class Answer < ActiveRecord::Base
 	has_many :comments, :as => :commentable
 	has_many :votes, :as => :votable
 
+	def voted_by?(user)
+	  votes.exists?(user: user)
+	end
+
 	
 	
 end
