427e3bea8db245ee1a83cb82813a5d3b5924bc69
willienn8254 <willienn@hotmail.com>
2016-03-24 09:42:40 -0500

    Validad que los campos de questions esten llenos

--- app/models/question.rb
+++ app/models/question.rb
@@ -17,6 +17,9 @@ class Question < ActiveRecord::Base
 	has_many :comments, :as => :commentable
 	has_many :votes, :as => :votable
 
+	validates :title, presence: true
+	validates :body, presence: true
+
 
 	def voted_by?(user)
 	  votes.exists?(user: user)
